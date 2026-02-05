ca9d950a380b820567a717744d9e96c0e983746e
Om Makhija <115364000+OmMakhija@users.noreply.github.com>
2026-02-05 20:34:22 +0530

    Time: 0 ms (100%), Space: 11.7 MB (100%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1=(int)(num1)
+        num2=(int)(num2)
+
+        num=num1*num2
+
+        num=str(num)
+
+        return num
+        
