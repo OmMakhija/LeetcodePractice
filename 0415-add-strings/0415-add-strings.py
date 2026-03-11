597139163241d94aa7a5314ab75b025fe859806f
Om Makhija <115364000+OmMakhija@users.noreply.github.com>
2026-03-11 21:52:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1=int(num1)
+        num2=int(num2)
+
+        num1=num1+num2
+
+        num1=str(num1)
+        return num1
